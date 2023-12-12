cca5dfdb21446b81b74ff8f5142a12e9018fd689
Drumstickz64 <gearman533@gmail.com>
2023-12-13 00:20:29 +0300

    complete day 5 part 1

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -32,9 +35,87 @@ func main() {
 }
 
 func part1(input string) {
-	panic("todo")
+	sections := strings.Split(input, "\r\n\r\n")
+	seedsStr := sections[0]
+	seedsStr = seedsStr[7:]
+	seeds := []int{}
+	for _, seedStr := range strings.Split(seedsStr, " ") {
+		seed, err := strconv.Atoi(seedStr)
+		if err != nil {
+			log.Fatalln("Failed to parse seed: ", err)
+		}
+		seeds = append(seeds, seed)
+	}
+	items := seeds
+	for sectionIdx := 1; sectionIdx < len(sections); sectionIdx++ {
+		section := sections[sectionIdx]
+		ranges := []NumberRange{}
+		sectionLines := strings.Split(section, "\r\n")
+		// ignore title
+		for i := 1; i < len(sectionLines); i++ {
+			ranges = append(ranges, parseRange(sectionLines[i]))
+		}
+		// map current items into their currosponding next item
+		for itemIdx := 0; itemIdx < len(items); itemIdx++ {
+			newItem := mapItemWithRanges(items[itemIdx], ranges)
+			items[itemIdx] = newItem
+		}
+	}
+
+	minLocation := slices.Min(items)
+	fmt.Println("Result: ", minLocation)
 }
 
 func part2(input string) {
 	panic("todo")
+}
+
+type NumberRange struct {
+	srcStart  int
+	destStart int
+	length    int
+}
+
+func (r *NumberRange) Contains(item int) bool {
+	return item >= r.srcStart && item <= r.srcStart+r.length
+}
+
+func (r *NumberRange) Map(item int) int {
+	diff := r.destStart - r.srcStart
+	return item + diff
+}
+
+func parseRange(rangeStr string) NumberRange {
+	rangeComponents := strings.Split(rangeStr, " ")
+	destStartStr := rangeComponents[0]
+	destStart, err := strconv.Atoi(destStartStr)
+	if err != nil {
+		log.Fatalln("Failed to parse destination range start: ", err)
+	}
+	srcStartStr := rangeComponents[1]
+	srcStart, err := strconv.Atoi(srcStartStr)
+	if err != nil {
+		log.Fatalln("Failed to parse source range start: ", err)
+	}
+	lengthStr := rangeComponents[2]
+	length, err := strconv.Atoi(lengthStr)
+	if err != nil {
+		log.Fatalln("Failed to parse range length: ", err)
+	}
+
+	return NumberRange{
+		srcStart:  srcStart,
+		destStart: destStart,
+		length:    length,
+	}
+}
+
+func mapItemWithRanges(item int, ranges []NumberRange) int {
+	for _, numRange := range ranges {
+		if numRange.Contains(item) {
+			return numRange.Map(item)
+		}
+	}
+
+	return item
 }
