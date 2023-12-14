43250687dfe32cb79bbe5bc93d705242199ce18c
Drumstickz64 <gearman533@gmail.com>
2023-12-15 00:13:41 +0300

    complete day 6 part 1

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -32,9 +35,48 @@ func main() {
 }
 
 func part1(input string) {
-	panic("todo")
+	lines := strings.Split(input, "\r\n")
+	timesStr := lines[0][5:]
+	distancesStr := lines[1][9:]
+	re := regexp.MustCompile(`\s*(\d+)`)
+	timesMatches := re.FindAllStringSubmatch(timesStr, -1)
+	distancesMatches := re.FindAllStringSubmatch(distancesStr, -1)
+
+	result := 1
+	for i := 0; i < len(timesMatches); i++ {
+		timeStr := timesMatches[i][1]
+		distanceStr := distancesMatches[i][1]
+
+		time, err := strconv.Atoi(timeStr)
+		if err != nil {
+			log.Fatalln("Failed to parse time: ", err)
+		}
+		recordDistance, err := strconv.Atoi(distanceStr)
+		if err != nil {
+			log.Fatalln("Failed to parse distance: ", err)
+		}
+
+		numWays := calculateNumWays(time, recordDistance)
+		result *= numWays
+	}
+
+	fmt.Println("Result: ", result)
 }
 
 func part2(input string) {
 	panic("todo")
+}
+
+func calculateNumWays(time int, recordDistance int) int {
+	numWays := 0
+
+	for i := 1; i < time; i++ {
+		timeRemaining := time - i
+		distanceTraveled := i * timeRemaining
+		if distanceTraveled > recordDistance {
+			numWays += 1
+		}
+	}
+
+	return numWays
 }
